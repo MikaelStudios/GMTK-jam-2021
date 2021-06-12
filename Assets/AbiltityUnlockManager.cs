6cc4dd2d9433d8430257fa44baa616867fb266f7
Micheal Fagbemi <magicmicheal4@gmail.com>
2021-06-12 13:55:05 +0100

    Color switching rules

---
+++ Assets/AbiltityUnlockManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class AbiltityUnlockManager : MonoBehaviour
+{
+    public List<AddedAbilityRules> Rules = new List<AddedAbilityRules>();
+
+    List<AbilityClass> m_currentAbilities = new List<AbilityClass>();
+    AbilityModuleManager abilityManager;
+    private void Start()
+    {
+        abilityManager = GetComponent<AbilityModuleManager>();
+    }
+    public void AddColor(AbilityClass ability)
+    {
+        if (m_currentAbilities.Contains(ability))
+            return;
+        m_currentAbilities.Add(ability);
+        Debug.Log("Added color " + ability.ToString());
+        if (m_currentAbilities.Count > 1)
+        {
+
+            for (int i = 0; i < Rules.Count; i++)
+            {
+
+                if (Rules[i].abilities.All(m_currentAbilities.Contains) && Rules[i].abilities.Count == m_currentAbilities.Count)
+                {
+                    Debug.Log("Applied ruled");
+                    abilityManager.ApplyAbilityUnlockList(Rules[i].abilityUnlock);
+                    SetColor(Rules[i].newColor.color);
+                    Rules[i].UnlockEvent?.Invoke();
+                    return;
+                }
+            }
+        }
+    }
+    public void SetColor(Color m_color)
+    {
+        SpriteRenderer spriteRenderer = transform.Find("SpriteTransformBase").transform.Find("SpriteAnimator").GetComponent<SpriteRenderer>();
+        spriteRenderer.color = m_color;
+        print(spriteRenderer.color.ToString());
+    }
+    [System.Serializable]
+    public class AddedAbilityRules
+    {
+        public List<AbilityClass> abilities = new List<AbilityClass>();
+        public AbilityUnlockList abilityUnlock;
+        public Material newColor;
+        public UnityEvent UnlockEvent;
+    }
+}
+public enum AbilityClass
+{
+    Red, Blue, Green
+}
--- Assets/CharacterEditorPackage/Code/AbilityUnlocks/AbilityUnlockTrigger.cs
+++ Assets/CharacterEditorPackage/Code/AbilityUnlocks/AbilityUnlockTrigger.cs
@@ -1,12 +1,14 @@
 ﻿using UnityEngine;
-using System.Collections;
 
-public class AbilityUnlockTrigger : MonoBehaviour {
+public class AbilityUnlockTrigger : MonoBehaviour
+{
     [SerializeField] AbilityUnlockList m_List = null;
     [SerializeField] bool m_DisableAfterTriggering = false;
+    [SerializeField] AbilityClass m_abilityClass;
     void OnTriggerEnter(Collider a_Collider)
     {
         AbilityModuleManager abilityManager = a_Collider.GetComponent<AbilityModuleManager>();
+        AbiltityUnlockManager unlockManager = a_Collider.GetComponent<AbiltityUnlockManager>();
         if (abilityManager)
         {
             abilityManager.ApplyAbilityUnlockList(m_List);
@@ -17,12 +19,13 @@ public class AbilityUnlockTrigger : MonoBehaviour {
         }
 
         Renderer renderer = GetComponent<Renderer>();
-        if(!renderer) return;
+        if (!renderer) return;
 
         Color color = renderer.material.color;
         print(color.ToString());
         SpriteRenderer spriteRenderer = a_Collider.transform.Find("SpriteTransformBase").transform.Find("SpriteAnimator").GetComponent<SpriteRenderer>();
         spriteRenderer.color = color;
         print(spriteRenderer.color.ToString());
+        unlockManager.AddColor(m_abilityClass);
     }
 }
