21a0f96151b56c61dd4b8c81029187be2e945223
Micheal Fagbemi <magicmicheal4@gmail.com>
2021-06-12 20:04:36 +0100

    Demo Scene done

--- Assets/Scripts/Triggers/SceneLoadTrigger.cs
+++ Assets/Scripts/Triggers/SceneLoadTrigger.cs
@@ -1,22 +1,32 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 /*Loads a new scene, while also clearing level-specific inventory!*/
 
 public class SceneLoadTrigger : MonoBehaviour
 {
-
+    public bool kit = true;
     [SerializeField] string loadSceneName;
 
     void OnTriggerEnter2D(Collider2D col)
     {
-        if (col.gameObject == NewPlayer.Instance.gameObject)
+        if (kit)
         {
-            GameManager.Instance.hud.loadSceneName = loadSceneName;
-            GameManager.Instance.inventory.Clear();
-            GameManager.Instance.hud.animator.SetTrigger("coverScreen");
-            enabled = false;
+            if (col.gameObject == NewPlayer.Instance.gameObject)
+            {
+                GameManager.Instance.hud.loadSceneName = loadSceneName;
+                GameManager.Instance.inventory.Clear();
+                GameManager.Instance.hud.animator.SetTrigger("coverScreen");
+                enabled = false;
+            }
         }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (kit)
+            return;
+        AbiltityUnlockManager unlockManager = other.GetComponent<AbiltityUnlockManager>();
+        if (unlockManager)
+            SceneManager.LoadScene(loadSceneName);
     }
 }
