ef3ab240a75b703617d6bc2250cd887732af9ed9
JL Euler <jeuler@mailbox.org>
2021-06-13 10:18:34 +0200

    add collect trigger
    TODO: add score

---
+++ Assets/CharacterEditorPackage/Code/Game/ExtraPlayerScripts/CollectTrigger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CollectTrigger : MonoBehaviour
+{
+    [SerializeField] private AudioSource m_AudioSource;
+    [SerializeField] private AudioClip[] m_CollectSounds;
+    [SerializeField] public UnityEvent m_CollectEvent;
+
+    void OnTriggerEnter(Collider a_Collider) {
+        if(a_Collider.gameObject.layer == 31)
+        {
+            // TODO: Add score
+            Collect();
+        }
+    }
+
+    void Collect() {
+        m_AudioSource?.PlayOneShot(m_CollectSounds[Random.Range(0, m_CollectSounds.Length)], Random.Range(.6f, 1f));
+        m_CollectEvent?.Invoke();
+
+        gameObject.SetActive(false);
+    }
+}
