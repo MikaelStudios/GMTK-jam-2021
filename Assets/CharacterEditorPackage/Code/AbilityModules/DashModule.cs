7804cea8de5c9e3f1cfadcb047e1775afd6c112d
JL Euler <jeuler@mailbox.org>
2021-06-13 13:48:39 +0200

    fix dash

--- Assets/CharacterEditorPackage/Code/AbilityModules/DashModule.cs
+++ Assets/CharacterEditorPackage/Code/AbilityModules/DashModule.cs
@@ -10,36 +10,39 @@ public class DashModule : GroundedControllerAbilityModule
     [SerializeField] bool m_ResetDashsAfterTouchingWall = false;
     [SerializeField] bool m_ResetDashsAfterTouchingEdge = false;
     [SerializeField] bool m_OverridePreviousSpeed = false;
+    [SerializeField] bool m_DisableGravity = false;
+    [SerializeField] bool m_DashFalloff = false;
+    [SerializeField] float m_FalloffDuration = 0.0f;
 
     [SerializeField] int amountofdash = 1;
 
-    float m_LastDashTime;
+    float m_DashCountdown = 0.0f;
     bool m_HasDashedAndNotTouchedGroundYet;
     int useddashes = 0;
     float originalGravity;
+
     private void Start()
     {
         originalGravity = m_CharacterController.Gravity;
     }
+
     //Reset all state when this module gets initialized
     protected override void ResetState()
     {
         base.ResetState();
-        m_LastDashTime = Time.fixedTime - m_DashCooldown;
         m_HasDashedAndNotTouchedGroundYet = false;
         useddashes = amountofdash;
-        //m_CharacterController.Gravity = originalGravity;
     }
 
     //Called whenever this module is started (was inactive, now is active)
     protected override void StartModuleImpl()
     {
-        m_LastDashTime = Time.fixedTime;
+        m_DashCountdown = m_DashCooldown;
         useddashes--;
         if (useddashes <= 0)
             m_HasDashedAndNotTouchedGroundYet = true;
-        m_CharacterController.Gravity = 0;
-
+        if(m_DisableGravity)
+            m_CharacterController.Gravity = 0.0f;
     }
 
     //Execute jump (lasts one update)
@@ -63,20 +66,36 @@ public class DashModule : GroundedControllerAbilityModule
     //Called whenever this module is inactive and updating (implementation by child modules), useful for cooldown updating etc.
     public override void InactiveUpdateModule()
     {
-        if (Time.fixedTime - m_LastDashTime < m_DashCooldown)
+        if(m_DashCountdown > -m_FalloffDuration)
+        {
+            m_DashCountdown -= Time.fixedDeltaTime;
+        }
+        if (m_DisableGravity && (m_CharacterController.Gravity == 0.0f) && (m_DashCountdown < 0.0f))
+        {
             m_CharacterController.Gravity = originalGravity;
+        }
         if (m_ControlledCollider.IsGrounded() ||
            (m_ControlledCollider.IsPartiallyTouchingWall() && m_ResetDashsAfterTouchingWall) ||
            (m_ControlledCollider.IsTouchingEdge() && m_ResetDashsAfterTouchingEdge))
         {
-
             m_HasDashedAndNotTouchedGroundYet = false;
             useddashes = amountofdash;
+        }
+
+        
+        if(m_DashFalloff && (m_DashCountdown < 0.0f) && (m_DashCountdown > -m_FalloffDuration))
+        {
+            Vector2 currentVel = m_ControlledCollider.GetVelocity();
+            float factor = 1 + (m_DashCountdown/m_FalloffDuration); // At this point the cooldown is negative!
+            
+            currentVel *= factor;
+
+            m_ControlledCollider.UpdateWithVelocity(currentVel);
         }
     }
     public bool CanStartDash()
     {
-        if (Time.fixedTime - m_LastDashTime < m_DashCooldown || m_HasDashedAndNotTouchedGroundYet || !GetDirInput("Aim").HasSurpassedThreshold() || useddashes <= 0)
+        if (m_DashCountdown > 0.0f || m_HasDashedAndNotTouchedGroundYet || !GetDirInput("Aim").HasSurpassedThreshold() || useddashes <= 0)
         {
             return false;
         }
@@ -87,7 +106,7 @@ public class DashModule : GroundedControllerAbilityModule
     //Called every frame when inactive (to see if it could be) and when active (to see if it should not be)
     public override bool IsApplicable()
     {
-        if (Time.fixedTime - m_LastDashTime < m_DashCooldown || m_HasDashedAndNotTouchedGroundYet || useddashes <= 0)
+        if (m_DashCountdown > 0.0f || m_HasDashedAndNotTouchedGroundYet || useddashes <= 0)
         {
             return false;
         }
