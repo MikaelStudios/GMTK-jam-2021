3cfd3fc5df866d8e731084a06d2470631134f612
JL Euler <jeuler@mailbox.org>
2021-06-13 11:34:14 +0200

    add ability clearer

---
+++ Assets/CharacterEditorPackage/Code/AbilityUnlocks/AbilityClassClearer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbilityClassClearer : MonoBehaviour
+{
+    void OnTriggerEnter(Collider a_Collider)
+    {
+        AbiltityUnlockManager unlockManager = a_Collider.GetComponent<AbiltityUnlockManager>();
+
+        unlockManager.ClearAbilities();
+    }
+}
